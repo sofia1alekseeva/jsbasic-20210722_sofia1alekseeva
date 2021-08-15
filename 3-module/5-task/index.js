7cf6b1b5f5cc00990ccf1b0ee71bf76e3e8e2c4d
Sofia Alekseeva <sofia1alekseeva@yandex.ru>
2021-08-15 17:10:54 +0300

    added tasks in 3-module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,8 @@
 function getMinMax(str) {
   // ваш код...
+  let numbers = str.split(' ').filter(Number)
+  let minmax = {};
+  minmax.min = Math.min(...numbers);
+  minmax.max = Math.max(...numbers);
+  return minmax;
 }
